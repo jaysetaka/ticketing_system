555bb3dca4466315657780cb72cecd9b27180e25
jabucwm <jabu@clockworkmedia.co.za>
2020-04-06 17:22:56 +0200

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,92 @@
+var nconf = require('nconf');
+var mongoose = require('mongoose');
+var winston = require('winston');
+
+var db = {};
+var mongoConnectionUri = {
+  server: process.env.TD_MONGODB_SERVER || nconf.get('mongo:host'),
+  port: process.env.TD_MONGODB_PORT || nconf.get('mongo:port') || '27017',
+  username: process.env.TD_MONGODB_USERNAME || nconf.get('mongo:username'),
+  password: process.env.TD_MONGODB_PASSWORD || nconf.get('mongo:password'),
+  database: process.env.TD_MONGODB_DATABASE || nconf.get('mongo:database'),
+  shard: process.env.TD_MONGODB_SHARD || nconf.get('mongo:shard')
+};
+
+var CONNECTION_URI = '';
+if (!mongoConnectionUri.username) {
+  CONNECTION_URI =
+    'mongodb://' + mongoConnectionUri.server + ':' + mongoConnectionUri.port + '/' + mongoConnectionUri.database;
+  if (mongoConnectionUri.shard === true)
+    CONNECTION_URI = 'mongodb+srv://' + mongoConnectionUri.server + '/' + mongoConnectionUri.database;
+} else {
+  mongoConnectionUri.password = encodeURIComponent(mongoConnectionUri.password);
+  if (mongoConnectionUri.shard === true)
+    CONNECTION_URI =
+      'mongodb+srv://' +
+      mongoConnectionUri.username +
+      ':' +
+      mongoConnectionUri.password +
+      '@' +
+      mongoConnectionUri.server +
+      '/' +
+      mongoConnectionUri.database;
+  else
+    CONNECTION_URI =
+      'mongodb://' +
+      mongoConnectionUri.username +
+      ':' +
+      mongoConnectionUri.password +
+      '@' +
+      mongoConnectionUri.server +
+      ':' +
+      mongoConnectionUri.port +
+      '/' +
+      mongoConnectionUri.database;
+}
+
+if (process.env.TD_MONGODB_URI) CONNECTION_URI = process.env.TD_MONGODB_URI;
+
+var options = {
+  keepAlive: 1,
+  connectTimeoutMS: 30000,
+  useNewUrlParser: true,
+  useCreateIndex: true
+};
+
+module.exports.init = function(callback, connectionString, opts) {
+  if (connectionString) CONNECTION_URI = connectionString;
+  if (opts) options = opts;
+  options.dbName = mongoConnectionUri.database;
+
+  if (db.connection) {
+    return callback(null, db);
+  }
+
+  global.CONNECTION_URI = CONNECTION_URI;
+
+  mongoose.Promise = global.Promise;
+  mongoose.set('useFindAndModify', false);
+  mongoose
+    .connect(CONNECTION_URI, options)
+    .then(function() {
+      if (!process.env.FORK) {
+        winston.info('Connected to MongoDB');
+      }
+
+      db.connection = mongoose.connection;
+      mongoose.connection.db.admin().command({ buildInfo: 1 }, function(err, info) {
+        if (err) winston.warn(err.message);
+        db.version = info.version;
+        return callback(null, db);
+      });
+    })
+    .catch(function(e) {
+      winston.error('Oh no, something went wrong with DB! - ' + e.message);
+      db.connection = null;
+
+      return callback(e, null);
+    });
+};
+
+module.exports.db = db;
+module.exports.connectionuri = CONNECTION_URI;
